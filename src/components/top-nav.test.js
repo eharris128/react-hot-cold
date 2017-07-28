80b6e29ab8167f448f6b566c75722c3525a10552
Lewi Gilamichael <dr.lewi@gmail.com>
2017-07-28 14:45:49 -0400

    added testing to top-nav.test.js

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import TopNav from "./top-nav";
 
 describe("<TopNav />", () => {
   it("Renders without crashing", () => {
+    //check props onInfo and onNewGame
     shallow(<TopNav />);
   });
+  
+  it('Should activate callback when clicked', () => {
+    const callback = jest.fn()
+    const wrapper = mount(<TopNav onInfo={callback} />)
+      wrapper.find('.what').simulate('click', { preventDefault() {} });
+      expect(callback).toHaveBeenCalled();
+  });
+
 });
