fafebe5de3e1e5fe508248fb601b17f019ab7f76
mscottcam <mscottcam@gmail.com>
2017-08-01 14:34:14 -0500

    game and guess-form ready

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -4,18 +4,8 @@ import {connect} from 'react-redux';
 import "./guess-form.css";
 
 export class GuessForm extends React.Component {
-  // onGuess(event) {
-  //   event.preventDefault();
-
-  //   if (this.props.onGuess) {
-  //     const value = this.input.value;
-  //     this.props.onGuess(value);
-  //   }
-  //   this.input.value = "";
-  // }
 
   render() {
-    // For user input: add to button: onClick={() => this.props.dispatch(myActionFunctionName())}
     return (
       <form onSubmit={e => e.preventDefault()}>
         <label htmlFor="userGuess">Enter your Guess</label>
@@ -36,17 +26,10 @@ export class GuessForm extends React.Component {
           className="button"
           name="submit"
           value="Guess"
-        />
+          onClick={() => this.props.dispatch(submit(parseInt(this.input.value, 10)))}/>
       </form>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  showInfo: state.showInfo,
-  correctAnswer: state.correctAnswer,
-  feedback: state.feedback,
-  guesses: state.guesses
-})
-
-export default connect(mapStateToProps)(GuessForm)
+export default connect()(GuessForm);
